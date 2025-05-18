fb219abd045eae9d4b4c88c8e972e7ca805c2b81
Rejeb Mohamed <rejebpsn@gmail.com>
2025-05-18 12:18:44 +0000

    اريد ان يكون الاسم كاملا

--- src/components/dashboard/app-logo.tsx
+++ src/components/dashboard/app-logo.tsx
@@ -5,7 +5,7 @@ export function AppLogo() {
   return (
     <Link href="/dashboard" className="flex items-center gap-2 text-lg font-semibold text-primary hover:text-primary/90 transition-colors">
       <Package className="h-7 w-7" />
-      <span className="font-bold">قسم التجهيز بالمتلوي</span>
+      <span className="font-bold">قسم التجهيز بمنطقة الحرس الوطني بالمتلوي</span>
     </Link>
   );
 }
